c56f9f7243520e8927231316647a77d3c6474de0
Iyashi <126176394+Iyashinouta@users.noreply.github.com>
2023-03-01 02:07:49 +0700

    Update model-downloader.py

--- scripts/model-downloader.py
+++ scripts/model-downloader.py
@@ -3,20 +3,23 @@ import gradio as gr
 from modules import scripts, script_callbacks
 from subprocess import getoutput
 
-def downloading (content_type, url):
-  if content_type == "Checkpoint":
-        return output + url + " -d /content/stable-diffusion-webui/models/Stable-diffusion" 
-  elif content_type == "Hypernetwork":
-        return output + url + " -d /content/stable-diffusion-webui/models/hypernetworks"
-  elif content_type == "TextualInversion/Embedding":
-        return output + url + " -d /content/stable-diffusion-webui/embeddings" 
-  elif content_type == "AestheticGradient":
-        return output + url + " -d /content/stable-diffusion-webui/extensions/stable-diffusion-webui-aesthetic-gradients/aesthetic_embeddings"
-  elif content_type == "VAE":
-        return output + url + " -d /content/stable-diffusion-webui/models/VAE"
-  elif content_type == "Lora":
-        return output + url + " -d /content/stable-diffusion-webui/models/Lora"
-    
+def folder(content_type):
+    if content_type == "Checkpoint":
+       return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/Stable-diffusion")
+    elif content_type == "Hypernetwork":
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/hypernetworks")
+    elif content_type == "TextualInversion/Embedding":
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/embeddings")
+    elif content_type == "AestheticGradient":
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/extensions/stable-diffusion-webui-aesthetic-gradients/aesthetic_embeddings")
+    elif content_type == "VAE":
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/VAE")
+    elif content_type == "Lora":
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/Lora")
+
+def split(downloader_type, url, in_text):
+  return gr.Textbox.update(downloader_type + url + in_text)
+
 def run(command):
     out = getoutput(f"{command}")
     return out
@@ -25,12 +28,14 @@ def on_ui_tabs():
     with gr.Blocks() as downloader:
         with gr.Group():
             with gr.Box():
-                content_type = gr.Radio(label='Content type:', choices=["Checkpoint","Hypernetwork","TextualInversion/Embedding","AestheticGradient", "VAE", "Lora"], value="Checkpoint", type="value")
+                content_type = gr.Radio(label='Content type:', choices=["Checkpoint","Hypernetwork","TextualInversion/Embedding","AestheticGradient", "VAE", "Lora"])
+                in_text = gr.Textbox(visible=False)
+                content_type.change(fn=folder, inputs=content_type, outputs=in_text)
                 url = gr.Textbox(label="Link Download", max_lines=1, placeholder="Type/Paste URL Here")
-                output = gr.Textbox(label="aria2c --console-log-level=error -c -x 16 -s 16 -k 1M ", visible=True, interactive=False)
-                comand = gr.Textbox(downloading, visible=True, interactive=False)
-                btn_run = gr.Button("Start Download")
-                btn_run.click(fn=downloading, inputs=command, outputs=out_text)
-                out_text + gr.Text
+                downloader_type = gr.Textbox(value="aria2c --console-log-level=error -c -x 16 -s 16 -k 1M ", visible=False, interactive=False)
+                commands = gr.Textbox(visible=False)
+                url.change(fn=split, inputs=[downloader_type, url, in_text], outputs=commands)
+                out_text = gr.Textbox(label="Result", placeholder="Result")
+                gr.Button("Start Download").click(fn=run, inputs=commands, outputs=out_text)
     return (downloader, "Model Downloader", "downloader"),
 script_callbacks.on_ui_tabs(on_ui_tabs)
