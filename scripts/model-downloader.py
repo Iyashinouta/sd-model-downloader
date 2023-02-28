6e6c26281365712bd4f19bf85da1e34f7f2ab79f
Iyashi <126176394+Iyashinouta@users.noreply.github.com>
2023-03-01 06:47:27 +0700

    Update model-downloader.py

--- scripts/model-downloader.py
+++ scripts/model-downloader.py
@@ -5,23 +5,23 @@ from subprocess import getoutput
 
 def folder(content_type):
     if content_type == "Checkpoint":
-       return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/Stable-diffusion")
+       return gr.update(value=" -d /content/stable-diffusion-webui/models/Stable-diffusion")
     elif content_type == "Hypernetwork":
-         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/hypernetworks")
+         return gr.update(value=" -d /content/stable-diffusion-webui/models/hypernetworks")
     elif content_type == "TextualInversion/Embedding":
-         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/embeddings")
+         return gr.update(value=" -d /content/stable-diffusion-webui/embeddings")
     elif content_type == "AestheticGradient":
-         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/extensions/stable-diffusion-webui-aesthetic-gradients/aesthetic_embeddings")
+         return gr.update(value=" -d /content/stable-diffusion-webui/extensions/stable-diffusion-webui-aesthetic-gradients/aesthetic_embeddings")
     elif content_type == "VAE":
-         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/VAE")
+         return gr.update(value=" -d /content/stable-diffusion-webui/models/VAE")
     elif content_type == "Lora":
-         return gr.Textbox.update(value=" -d /content/stable-diffusion-webui/models/Lora")
+         return gr.update(value=" -d /content/stable-diffusion-webui/models/Lora")
 
 def combine(downloader_type, url, content_type1, file_name):
-    return gr.Textbox.update(downloader_type + url + content_type1 + file_name)
+    return gr.update(downloader_type + url + content_type1 + file_name)
 
 def combine1(downloader_type, url, content_type1, file_name1, file_name):
-    return gr.Textbox.update(downloader_type + url + content_type1 + file_name1 + file_name)
+    return gr.update(downloader_type + url + content_type1 + file_name1 + file_name)
 
 def run(command):
     out = getoutput(f"{command}")
@@ -31,21 +31,22 @@ def on_ui_tabs():
     with gr.Blocks() as downloader:
         with gr.Group():
             with gr.Box():
-                content_type = gr.Radio(label="Content type:", choices=["Checkpoint","Hypernetwork","TextualInversion/Embedding","AestheticGradient", "VAE", "Lora"])
-                content_type1 = gr.Textbox(visible=False)
-                content_type.change(fn=folder, inputs=content_type, outputs=content_type1)
                 with gr.Row():
-                    url = gr.Textbox(label="Link Download", max_lines=1, placeholder="Type/Paste URL Here")
-                    file_name = gr.Textbox(label="File Name(Required if Download URL from HuggingFace)", placeholder="Type/Input Filename.extension Here", interactive=True)
+                    content_type = gr.Radio(label="1. Choose Content type", choices=["Checkpoint","Hypernetwork","TextualInversion/Embedding","AestheticGradient", "VAE", "Lora"])
+                    content_type1 = gr.Textbox(visible=False)
+                    content_type.change(fn=folder, inputs=content_type, outputs=content_type1)
+                    download_btn = gr.Button("Start Download")
+                with gr.Row():
+                    url = gr.Textbox(label="2. Put Link Download Below", max_lines=1, placeholder="Type/Paste URL Here")
+                    file_name = gr.Textbox(label="3. Set File Name(Required if Download URL from HuggingFace)", placeholder="Type/Input Filename.extension Here", interactive=True)
                     file_name1 = gr.Textbox(value=" -o ", visible=False)
                 downloader_type = gr.Textbox(value="aria2c --console-log-level=error -c -x 16 -s 16 -k 1M ", visible=False, interactive=False)
-                commands = gr.Textbox(visible=False)
+                commands = gr.Textbox(label="Command", visible=True, interaactive=False, show_progress=True)
                 url.change(fn=combine, inputs=[downloader_type, url, content_type1, file_name], outputs=commands)
                 file_name.change(fn=combine1, inputs=[downloader_type, url, content_type1, file_name1, file_name], outputs=commands)
                 content_type1.change(fn=combine, inputs=[downloader_type, url, content_type1, file_name], outputs=commands)
-                with gr.Row():
-                    download_btn = gr.Button("Start Download")
-                    out_text = gr.Textbox(label="Result", placeholder="Result")
-                    download_btn.click(fn=run, inputs=commands, outputs=out_text)
+                out_text = gr.Textbox(label="Result", placeholder="Result", scroll_to_output=True)
+                download_btn.click(fn=run, inputs=commands, outputs=out_text)
     return (downloader, "Model Downloader", "downloader"),
+    
 script_callbacks.on_ui_tabs(on_ui_tabs)
