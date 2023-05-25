e94db27fd583e96ea67e71a6727f901555b7193a
Iyashi <126176394+Iyashinouta@users.noreply.github.com>
2023-05-26 02:56:45 +0700

    Update model-downloader.py

--- scripts/model-downloader.py
+++ scripts/model-downloader.py
@@ -1,13 +1,11 @@
 import os
-import json
 import requests
-import werkzeug
 import gradio as gr
 from urllib.parse import urlparse
 from modules import script_callbacks
 
 sd_path = os.getcwd()
-default = "Set as Default"
+no_prev = f"{sd_path}/html/card-no-preview.png"
 checkpoint_path = "/models/Stable-diffusion"
 hypernetwork_path = "/models/hypernetworks"
 embedding_path = "/embeddings"
@@ -43,99 +41,88 @@ else:
 print("Model Downloader: all Directories already Created.")
 
 def folder(content_type):
+    global downloadpath
+    global namepath
+    global modelname
+    downloadpath = None
+    namepath = None
+    modelname = None
     if content_type == "Checkpoint":
-       return gr.Textbox.update(value=checkpoint_path)
+       downloadpath = checkpoint_path
+       return downloadpath
     elif content_type == "Hypernetwork":
-         return gr.Textbox.update(value=hypernetwork_path)
+         downloadpath = hypernetwork_path
+         return downloadpath
     elif content_type == "TextualInversion/Embedding":
-         return gr.Textbox.update(value=embedding_path)
+         downloadpath = embedding_path
+         return downloadpath
     elif content_type == "AestheticGradient":
-         return gr.Textbox.update(value=aestheticembedding_path)
+         downloadpath = aestheticembedding_path
+         return downloadpath
     elif content_type == "VAE":
-         return gr.Textbox.update(value=vae_path)
+         downloadpath = vae_path
+         return downloadpath
     elif content_type == "LoRA":
-         return gr.Textbox.update(value=lora_path)
+         downloadpath = lora_path
+         return downloadpath
     elif content_type == "LyCORIS(LoCon/LoHA)":
-         return gr.Textbox.update(value=lycoris_path)
+         downloadpath = lycoris_path
+         return downloadpath
 
-def get_filename_from_url(url=None):
-    if url is None:
-       return None
-    else:
-         try:
-             with requests.get(url, stream=True) as req:
-                  if content_disposition := req.headers.get("Content-Disposition"):
-                     param, options = werkzeug.http.parse_options_header(content_disposition)
-                     if param == 'attachment' and (filename := options.get('filename')):
-                        f = filename.replace(" ", "_")
-                        return f
-                  path = urlparse(req.url).path
-                  n = path[path.rfind('/') + 1:]
-                  name = n.replace(" ", "_")
-                  return name
-         except:
-                return default
-
-def change_filename(filename1, filename):
-    if filename1 == "Use original Filename from the Source":
-       return gr.Textbox(filename).update(visible=False)
-    elif filename1 == "Create New Filename(Recomended)":
-         return gr.Textbox(filename).update(visible=True)
-
-def combine(url, content_type1, filename):
-    global pathname
+def get_data_from_url(url, name, image, download_btn, out_text):
+    global namepath
     global modelurl
     global modelname
-    global downloadpath
-    pathname = os.path.splitext(filename)[0]
+    global imgurl
+    global imgname
+    namepath = None
+    modelname = None
+    try:
+        if url.find("https://civitai.com/")!=-1:
+           convert = "" + url.replace("download/models", "v1/model-versions")
+           req = requests.get(convert, stream=True)
+           metadata1 = req.json()['files'][0]['name']
+           metadata1.replace(" ", "_")
+           metadata2 = req.json()['images'][0]['url']
+           modelname = metadata1
+           namepath = os.path.splitext(metadata1)[0]
+           imgurl = metadata2
+           imgname = f"{namepath}.preview.png"
+           return gr.Textbox(name).update(value=modelname), gr.Image(image).update(value=imgurl), gr.Button(download_btn).update(visible=True, variant="primary"), gr.Textbox(out_text).update(value= "Ready", visible=True)
+        else:
+             parse = urlparse(url).path
+             metadata1 = parse[parse.rfind('/') + 1:]
+             metadata1.replace(" ", "_")
+             modelname = metadata1
+             namepath = os.path.splitext(metadata1)[0]
+             imgurl = no_prev
+             imgname = f"{namepath}.preview.png"
+             return gr.Textbox(name).update(value=modelname), gr.Image(image).update(value=imgurl), gr.Button(download_btn).update(visible=True, variant="primary"), gr.Textbox(out_text).update(value= "Ready", visible=True)
+    except:
+           imgurl = no_prev
+           return gr.Image(image).update(value=imgurl)
+
+def info_update(url, content_type):
     modelurl = "" + url
-    modelname = "" + filename
-    downloadpath = "" + content_type1
-    return gr.Textbox.update(value=f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --input-file model.txt -d {sd_path}{content_type1}/{pathname}")
+    return gr.Markdown.update(f"<font size=2><p><b>Model Information :</b><br><b>URL</b>:  {modelurl} <br> <b>Folder Path</b>:  {downloadpath}/{namepath} <br> <b>File Name</b>:  {modelname} <br> <b>Preview Model</b>:</p>")
 
-def info_update(url, content_type1, filename, info):
-    return gr.Markdown.update(f"<font size=2><p><b>URL</b>:  {url} <br> <b>Folder Path</b>:  {content_type1} <br> <b>File Name</b>:  {filename} <br> <b>Preview Model</b>:</p>")
+def show_filename(filename1):
+    if filename1 == "Use original Filename from the Source(Recomended)":
+       return gr.Textbox.update(visible=False)
+    elif filename1 == "Create New Filename":
+         return gr.Textbox.update(visible=True)
 
-def get_image_from_url(url):
-    convert = "" + url.replace("download/models", "v1/model-versions")
-    civitai = "https://image.civitai.com/"
-    if not modelurl.find("https://civitai.com/")!=-1:
-       return gr.Image.update(value=f"{sd_path}/html/card-no-preview.png")
-    else:
-         try:
-             with requests.get(convert, stream=True) as req:
-                  j = req.json()
-                  r = json.dumps(j)
-                  start = r.find(civitai) + len(civitai)
-                  end = r.find('"', start)
-                  global img_url
-                  img_url = f"{civitai}{r[start:end]}"
-                  return gr.Image.update(value=img_url)
-         except:
-                return gr.Image.update(value=f"{sd_path}/html/card-no-preview.png")
-
-def show_download(filename):
-    a = gr.Button.update(visible=True, variant="primary")
-    b = gr.Textbox.update(value= "Ready", visible=True)
-    c = gr.Markdown.update(visible=True)
-    return a, b, c
-
-def back (download_button):
-    return gr.Button.update(visible=True, variant="secondary")
-
-success = "Download Completed, Saved to"
-exist = "File Already Exist in"
-
-def run(command):
-    imgname = f"{pathname}.preview.png"
-    complete1 = f"SUCCESS: {success} [{sd_path}{downloadpath}/{pathname}]"
-    complete2 = f"ERROR: {exist} [{sd_path}{downloadpath}/{pathname}]"
+def start_downloading(download_btn, url, name):
+    modelurl = "" + url
+    command = f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --input-file model.txt -d {sd_path}{downloadpath}/{namepath}"
+    complete1 = f"SUCCESS: Download Completed, Saved to\n[{sd_path}{downloadpath}/{namepath}]"
+    complete2 = f"ERROR: File Already Exist in\n[{sd_path}{downloadpath}/{namepath}]"
     with open("model.txt", "w") as w:
          if not modelurl.find("https://civitai.com/")!=-1:
             w.write(f"{modelurl}\n out={modelname}")
          else:
-              w.write(f"{modelurl}\n out={modelname}\n{img_url}\n out={imgname}")
-    if not os.path.exists(f"{sd_path}{downloadpath}/{pathname}"):
+              w.write(f"{modelurl}\n out={modelname}\n{imgurl}\n out={imgname}")
+    if not os.path.exists(f"{sd_path}{downloadpath}/{namepath}"):
        line1 = os.popen(command)
        for l in line1:
            l = l.rstrip()
@@ -145,20 +132,21 @@ def run(command):
          yield complete2
          print(complete2)
 
+def back(download_btn):
+    return gr.Button.update(visible=True, variant="secondary")
+
 def on_ui_tabs():
     with gr.Blocks() as downloader:    
          with gr.Row():
               with gr.Column(scale=2):
                    content_type = gr.Radio(label="1. Choose Content type", choices=["Checkpoint","Hypernetwork","TextualInversion/Embedding","AestheticGradient", "VAE", "LoRA", "LyCORIS(LoCon/LoHA)"])
-                   content_type1 = gr.Textbox(visible=False)
          with gr.Row():
               url = gr.Textbox(label="2. Put Link Download Below", max_lines=1, placeholder="Type/Paste URL Here")
               url.style(show_copy_button=True)
          with gr.Row():
-              filename1 = gr.Radio(label="Setting Filename", choices=["Use original Filename from the Source", "Create New Filename(Recomended)"], type="value", value="Use original Filename from the Source")
+              filename1 = gr.Radio(label="Setting Filename(please choose original, we're getting bug on 'Create New Filename')", choices=["Use original Filename from the Source(Recomended)", "Create New Filename"], type="value", value="Use original Filename from the Source(Recomended)")
          with gr.Row():
-              filename = gr.Textbox(label="3. Create new Filename", placeholder="Type/Paste Filename.extension Here", visible=False, interactive=True)
-              commands = gr.Textbox(visible=False, interactive=False)
+              name = gr.Textbox(label="3. Create new Filename", placeholder="Type/Paste Filename.extension Here", visible=False, interactive=False)
          with gr.Row():
               download_btn = gr.Button("Start Download", visible=False, variant="secondary")
               out_text = gr.Textbox(label="Download Result", placeholder="Result", visible=False, show_progress=True)
@@ -166,33 +154,24 @@ def on_ui_tabs():
               with gr.Column():
                    info = gr.Markdown(value="<font size=2><p><b>Model Information :</b><br><b>URL</b>: <br> <b>Folder Path</b>: <br> <b>File Name</b>:<br> <b>Preview Model</b>:</p>", label="Information")
               with gr.Column():
-                   image = gr.Image(value=f"{sd_path}/html/card-no-preview.png", show_label=False)
+                   image = gr.Image(value=no_prev, show_label=False)
                    image.style(width=256, height=384)
          with gr.Row():
               github = gr.Markdown(
                """
                <center><font size=2>Having Issue? | <a href=https://github.com/Iyashinouta/sd-model-downloader/issues>Report Here</a>
-               """,
-               visible=False
+               """
               )
 
-              filename1.change(change_filename, filename1, filename)
-              content_type.change(folder, content_type, content_type1)
-              content_type1.change(combine, [url, content_type1, filename], commands)
-              url.change(combine, [url, content_type1, filename], commands)
-              url.change(get_filename_from_url, url, filename)
-              url.change(get_image_from_url, url, image)
-              filename.change(combine, [url, content_type1, filename], commands)
-              content_type1.change(info_update, [url, content_type1, filename], info)
-              url.change(info_update, [url, content_type1, filename], info)
-              filename.change(info_update, [url, content_type1, filename], info)
-              filename.change(show_download, filename, [download_btn, out_text, github])
+              filename1.change(show_filename, filename1, name)
+              content_type.change(folder, content_type)
+              url.change(get_data_from_url, url, [name, image, download_btn, out_text])
+              content_type.change(info_update, content_type, info)
+              url.change(info_update, [url, name], info)
+              download_btn.click(start_downloading, [download_btn, url, name], out_text)
+              url.submit(start_downloading, [download_btn, url, name], out_text)
               download_btn.click(back, download_btn, download_btn)
-              download_btn.click(run, commands, out_text)
               url.submit(back, url, download_btn)
-              url.submit(run, commands, out_text)
-              filename.submit(back, filename, download_btn)
-              filename.submit(run, commands, out_text)
 
     downloader.queue(concurrency_count=5)
     return (downloader, "Model Downloader", "downloader"),
