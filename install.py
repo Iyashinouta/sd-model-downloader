75580b7ec2b28f4dfd5cf6ee7304cc22db93fc53
Iyashi <126176394+Iyashinouta@users.noreply.github.com>
2023-06-08 23:10:10 +0700

    Update install.py

--- install.py
+++ install.py
@@ -3,6 +3,6 @@ import platform
 
 if platform.system() == "Windows":
    if not launch.is_installed("aria2"):
-      launch.run_pip("install aria2", "requirements for Model Downloader")
+      launch.run_python("apt -y install -qq aria2", "requirements for Model Downloader")
    if not launch.is_installed("wget"):
       launch.run_pip("install wget", "requirements for Model Downloader")
